653a605861768f2bab24a8fdeb1e345c6bdbdf33
Anderson Dantas <adantas@national.aaa.com>
2024-04-04 12:49:11 -0300

    3. Improve string handleling and map DS

--- src/bin/3_heap_allocator_flags_pgc/main.rs
+++ src/bin/3_heap_allocator_flags_pgc/main.rs
@@ -2,9 +2,10 @@
 // static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-use std::{collections::BTreeMap, env, fs::File, io::{BufRead, BufReader}};
+use std::{collections::HashMap, env, fs::File, io::{BufRead, BufReader}};
 
 use rand_distr::num_traits::ToPrimitive;
+use itertools::Itertools;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,25 +18,22 @@ fn main() {
     let file = File::open(filepath).unwrap();
     let reader = BufReader::new(file);
     
-    let mut map: BTreeMap<String, (f32, f32, f32, i32)> = BTreeMap::new();
+    let mut map: HashMap<String, (f32, f32, f32, i32)> = HashMap::new();
     for line in reader.lines() {
         let liner = line.unwrap();
         let lines: Vec<&str> = liner.split(";").collect();
-        let (name, value) = (lines[0], lines[1]);
+        let v = lines[1].parse::<f32>().unwrap(); 
         
-        map.entry(name.to_string()).or_insert( (f32::MAX, f32::MIN, 0.0, 0));
-        let v = value.parse::<f32>().unwrap(); 
-        
-        let station = map.get_mut(name).unwrap();
+        let station = map.entry(lines[0].to_string()).or_insert( (f32::MAX, f32::MIN, 0.0, 0));
         station.0 = station.0.min(v);
         station.1 = station.1.max(v);
         station.2 = station.2 + v;
         station.3 = station.3 + 1;
     }
 
-    // Compute solution
-    for (name, (min, max, sum, count)) in map {
+    for key in map.keys().sorted() {
+        let (min, max, sum, count) = map[key];
         let mean = sum / count.to_f32().unwrap();
-        println!("{name}={min:.1}/{max:.1}/{mean:.1}");
+        println!("{key}={min:.1}/{max:.1}/{mean:.1}");
     }
 }
