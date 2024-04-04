af473d0383594617a37e1342b2cf3baca941898a
Anderson Dantas <a.kleber.d@gmail.com>
2024-04-04 01:34:28 -0300

    3. benchmark PGC and add build flags

--- src/bin/3/main.rs
+++ src/bin/3/main.rs
@@ -1,5 +1,6 @@
 #[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+// static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::{collections::BTreeMap, env, fs::File, io::{BufRead, BufReader}};
 
