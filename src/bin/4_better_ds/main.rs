78e2c0ab8205cecb82341f00f39769466154dbb0
Anderson Dantas <a.kleber.d@gmail.com>
2024-04-12 00:26:48 -0300

    4. use FxHashMap

--- src/bin/4_better_ds/main.rs
+++ src/bin/4_better_ds/main.rs
@@ -3,15 +3,12 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 extern crate fxhash;
-use fxhash::{FxHashMap};
-use std::{env, fs::File, io::{BufRead, BufReader}};
+use fxhash::FxHashMap;
+use std::{collections::HashMap, env, fs::File, io::{BufRead, BufReader}, thread};
 use itertools::Itertools;
 
-const MIN_MAX_OFFSET: i128 = 1000;
-const MASK_16: i128 = 0xFFFF;
-const MASK_32: i128 = 0xFFFFFFF;
-
-fn eval_char(c: char) -> i32 {
+#[inline(always)]
+fn eval_char(c: char) -> i64 {
     match c {
         '0' => 0,
         '1' => 1,
@@ -26,7 +23,9 @@ fn eval_char(c: char) -> i32 {
         _ => 10,
     }
 }
-fn parse_measure(txt: &str) -> i128 {
+
+#[inline(always)]
+fn parse_measure(txt: &str) -> i64 {
     let mut ans = 0;
     let mut neg= false;
     for c in txt.chars() {
@@ -37,9 +36,9 @@ fn parse_measure(txt: &str) -> i128 {
         }
     }
     if neg {
-        return -ans as i128;
+        return -ans as i64;
     } else {
-        return ans as i128;
+        return ans as i64;
     }
 }
 
@@ -54,40 +53,38 @@ fn main() {
     let file = File::open(filepath).unwrap();
     let reader = BufReader::new(file);
 
-    // let mut map: FxHashMap<String, [i64; 4]> = FxHashMap::with_capacity_and_hasher(100000, Default::default());
-    let mut map: FxHashMap<String, i128> = FxHashMap::with_capacity_and_hasher(100000, Default::default());
-    // let mut map: HashMap<String, (i32, i32, i128, i32)> = HashMap::with_capacity_and_hasher(100000, Default::default());
+    let mut map: FxHashMap<String, [i64; 4]> = FxHashMap::with_capacity_and_hasher(10000, Default::default());
+    
     for line in reader.lines() {
         let liner = line.unwrap();
-        let lines: Vec<&str> = liner.split(";").collect();
-        let v = parse_measure(lines[1]);
+        let l: Vec<&str> = liner.split(";").collect();
+        let v = parse_measure(l[1]);
         
-        // let station = map.entry(lines[0].to_string()).or_insert( [i64::MAX, i64::MIN, 0, 0]);
-        // station[0] = station[0].min(v);
-        // station[1] = station[1].max(v);
-        // station[2] = station[2] + v;
-        // station[3] = station[3] + 1;
-        let station = map.entry(lines[0].to_string()).or_insert( 0xFFFF);
-        let min = i128::min(v + MIN_MAX_OFFSET, MASK_16 & *station);
-        let max = i128::min(v + MIN_MAX_OFFSET, MASK_16 & (*station >> 16));
-        let count = (MASK_32 & (*station >> 32)) +1;
-        let sum = (*station >> 64)+v;
-        *station = (sum << 64) | (count << 32) | (max << 16) | (min);
+        let station = map.entry(l[0].to_string()).or_insert( [i64::MAX, i64::MIN, 0, 0]);
+        station[0] = station[0].min(v);
+        station[1] = station[1].max(v);
+        station[2] = station[2] + v;
+        station[3] = station[3] + 1;
     }
 
-    // for key in map.keys().sorted() {
-    //     let [min, max, sum, count] = map[key];
-    //     let mean = (sum as f32) / (count as f32) /10.0;
-    //     println!("{key}={:.1}/{:.1}/{mean:.1}", min as f32 /10.0, max as f32 /10.0);
+    // let mut hlds = vec![];
+
+    // for i in 0..8 {
+    //     let t = thread::spawn(move || {
+    //         (move |j: i32| {
+    //             println!("{}", j);
+    //             return Box::new([j])
+    //         })(i);   
+    //     });
+    //     hlds.push(t);
+    // }
+
+    // for t in hlds {
+    //     _ = t.join();
     // }
 
     for key in map.keys().sorted() {
-        let station = map[key];
-        let min = (MASK_16 & station) -MIN_MAX_OFFSET;
-        let max = (MASK_16 & (station >> 16)) -MIN_MAX_OFFSET;
-        let count = MASK_32 & (station >> 32);
-        let sum = station >> 64;
-
+        let [min, max, sum, count] = map[key];
         let mean = (sum as f32) / (count as f32) /10.0;
         println!("{key}={:.1}/{:.1}/{mean:.1}", min as f32 /10.0, max as f32 /10.0);
     }
